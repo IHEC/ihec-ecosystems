6dec00cc9fabc89e7ec0351f56be021dc13016a5
sitag <usergatis@gmail.com>
2015-03-18 14:46:09 -0700

    ...

--- BCGSC_CEMT/MinimalPyUtils/__init__.py
+++ BCGSC_CEMT/MinimalPyUtils/__init__.py
@@ -1,16 +1,9 @@
 from Clargs import Clargs
-#from Reports import HashListHTML, JsonHtml, XMLReport
-#from JsonUtils import Json
-#from Shell import Shell, SAMtools, OS
-#from SQLiteConx import SQLiteConx
-#from Utils import Cmn, Par
-#from CEMTs import CEMTs
-#from LinkTrace import LinkTrace
-#from XMLJson import XMLJson
+from Utils import Cmn
 
 """ factor out minimal objects from PyUtils to hold all this together """
 def version():
-	return "minimalPyUtils 0.x"
+	return "MinimalPyUtils 0.x"
 
 if __name__ =="__main__":
 	print version()
