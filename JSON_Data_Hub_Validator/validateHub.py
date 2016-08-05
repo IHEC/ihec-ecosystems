bc90df1553b7e53455aa967ed49af09373494a67
dbujold <david.bujold@mail.mcgill.ca>
2016-08-05 12:15:16 -0400

    Implemented loose track type validation, and a less verbose validator output.

--- JSON_Data_Hub_Validator/validateHub.py
+++ JSON_Data_Hub_Validator/validateHub.py
@@ -1,34 +1,73 @@
+from __future__ import print_function
 from jsonschema import validate, exceptions, FormatChecker
 from sys import argv
 import json
 import os
+import getopt
+# import sys
 
 
-def validateJson(jsonObj):
-    """Validate a data hub against the IHEC Data Hub Schema"""
-
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/data_hub_schema.json') as jsonStr:
-        data = json.load(jsonStr)
-
-    return validate(jsonObj, data, format_checker=FormatChecker())
-
-
-def main():
+def main(argv):
     """Command line way to valide a data hub"""
-    script, jsonFilename = argv
 
-    with open(jsonFilename) as jsonFile:
-        jsonObj = json.load(jsonFile)
+    opts, args = getopt.getopt(argv, "", ["json=", "loose-validation", "verbose"])
+
+    json_filename = ''
+    is_loose_validation = False
+    is_verbose = False
+
+    for opt, arg in opts:
+        if opt == '--json':
+            json_filename = arg
+        elif opt == '--loose-validation':
+            is_loose_validation = True
+        elif opt == '--verbose':
+            is_verbose = True
+
+    #JSON to be validated is mandatory, otherwise, display options
+    if json_filename == '':
+        printHelp()
+        exit()
+
+    #If loose validation, use loose JSON Schema
+    if is_loose_validation:
+        schema_file = os.path.dirname(os.path.realpath(__file__)) + '/data_hub_schema_loose.json'
+    else:
+        schema_file = os.path.dirname(os.path.realpath(__file__)) + '/data_hub_schema.json'
+
+    with open(json_filename) as json_file:
+        jsonObj = json.load(json_file)
 
     try:
-        validateJson(jsonObj)
-        print "Data hub is valid."
+        validateJson(jsonObj, schema_file)
+        print("Data hub is valid.")
 
     except exceptions.ValidationError as e:
-        print "--------------------------------------------------"
-        print "- Validation error"
-        print "--------------------------------------------------"
-        print e
+        print("--------------------------------------------------")
+        print("- Validation error")
+        print("--------------------------------------------------")
+        if is_verbose:
+            print(e)
+        else:
+            try:
+                c = e.context.pop()
+                print(c.message)
+            except IndexError as ie:
+                print(e.message)
+
+
+def printHelp():
+    print("Usage: python validateHub.py --json=doc.json [--loose-validation --verbose]")
+
+
+def validateJson(jsonObj, schema_file):
+    """Validate a data hub against the IHEC Data Hub Schema"""
+
+    with open(schema_file) as jsonStr:
+        json_schema = json.load(jsonStr)
+
+    return validate(jsonObj, json_schema, format_checker=FormatChecker())
+
 
 if __name__ == "__main__":
-    main()
+    main(argv[1:])
