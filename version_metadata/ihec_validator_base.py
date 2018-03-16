9bb84a52b4fd7298f850880fdb6282d1968f8387
sitag <sgakkhar@bcgsc.ca>
2018-03-15 18:32:52 -0700

    fix bad tabs

--- version_metadata/ihec_validator_base.py
+++ version_metadata/ihec_validator_base.py
@@ -6,22 +6,26 @@ class IHECJsonValidator(object):
 		self.validators = validators
 
 	def is_valid_ihec(self):
-			validated = list()
-			for (xml, attrs) in self.xmljson:
-					version = self.latest_valid_version(attrs)
-					if version:
-							validated.append((version, xml))
-			return validated
+		validated = list()
+		for (xml, attrs) in self.xmljson:
+			version = self.latest_valid_version(attrs)
+			if version:
+				validated.append((version, xml))
+		return validated
 
 	def latest_valid_version(self, attributes):
-			attrs = attributes['attributes']
-			for version in self.validators:
-					validator = self.validators[version]
-					valid = validator.validate(attrs, details=attributes)
-					#print attrs.keys()
-					logger("# is valid ihec spec:{0} version:{1} [{2}]\n".format(valid, version if valid else '__invalid__', attributes['title']))
-					if valid:
-							return version
-			return None
+		attrs = attributes['attributes']
+		for version in self.validators:
+			validator = self.validators[version]
+			valid = validator.validate(attrs, details=attributes)
+			#print attrs.keys()
+			logger("# is valid ihec spec:{0} version:{1} [{2}]\n".format(valid, version if valid else '__invalid__', attributes['title']))
+			if valid:
+				return version
+		return None
 
 
+
+
+def validate():
+	return False
