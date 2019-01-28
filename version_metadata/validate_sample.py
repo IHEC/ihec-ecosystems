5ff5b1371a877ce25d2c719ddd69ddeedda39fe9
sitag <sgakkhar@bcgsc.ca>
2019-01-27 19:16:45 -0800

    added "-not-sra-xml-but-try" option

--- version_metadata/validate_sample.py
+++ version_metadata/validate_sample.py
@@ -56,7 +56,7 @@ def main(args):
 		sra = SRAParseObjSet.from_file(e)
 		nObjs += sra.nOffspring()
 		assert  sra.xml.getroot().tag  == objset, ['__Expected:' + objset]
-		assert sra.is_valid__xml(xml_validator)
+		assert sra.is_valid__xml(xml_validator) or args.has('-not-sra-xml-but-try')
 		v = SampleValidator(sra, ihec_validators)
 		validated.extend(v.is_valid_ihec())
 
