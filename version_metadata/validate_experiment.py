b813daca24176a39f8c8463e03bf5b25cf4bf373
sitag <sgakkhar@bcgsc.ca>
2020-02-18 01:41:34 -0800

    add separate json log

--- version_metadata/validate_experiment.py
+++ version_metadata/validate_experiment.py
@@ -84,7 +84,11 @@ def main(args):
 		logger('ok\n')
 	else:
 		logger('..no valid objects found\n')
-	
-	json2.pp({ e : v.errorlog  for e, v in expvalidator.items()})
+
+	errlog = { e : v.errorlog  for e, v in expvalidator.items()}
+	if args.has('-jsonlog'):
+		print(json2.dumpf(args['-jsonlog'], errlog)
+	else:
+		json2.pp(errlog)
 
 
