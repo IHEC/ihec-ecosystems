f04493800ae1c96dbb9dfe9e502bc917c064db98
sitag <->
2020-03-20 03:28:08 -0400

    hook in hack.load_schema

--- version_metadata/hack.py
+++ version_metadata/hack.py
@@ -1,4 +1,14 @@
-from .utils import json2
+from .utils import json2, cmn
+import os, sys
+import json 
 
-def load_schema(f):
-	return json2.loadf(f)
+def load_schema(f, ):
+	base = os.path.dirname(os.path.abspath(__file__))
+	cwd = os.getcwd()
+	expectedpath = 'file:./schemas/json/' 
+	newpath = 'file:{0}/../schemas/json/'.format(base)
+	schema_json = cmn.fread(f)
+	schema_json_fixed = schema_json.replace(expectedpath, newpath)
+	print(newpath, expectedpath)
+	assert schema_json_fixed.find(newpath) != -1
+	return json.loads(schema_json_fixed)
