3f23822c12cf419c2d55a7ddc48bd791d45182c7
sitag <->
2020-03-24 00:22:13 -0400

    fix  relative paths

--- version_metadata/hack.py
+++ version_metadata/hack.py
@@ -5,7 +5,7 @@ import json
 def load_schema(f):
 	base = os.path.dirname(os.path.abspath(__file__))
 	cwd = os.getcwd()
-	expectedpath = 'file:./schemas/json/' 
+	expectedpath = 'file:../schemas/json/' 
 	newpath = 'file:{0}/../schemas/json/'.format(base)
 	schema_json = cmn.fread(f)
 	schema_json_fixed = schema_json.replace(expectedpath, newpath)
