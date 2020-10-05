cbe875d6533f82416388f1c52c5206e8d601d4d9
sitag <sgakkhar@bcgsc.ca>
2020-10-05 04:09:36 -0700

    add rule stub for umi_read_structure

--- version_metadata/exp_semantic_rules.py
+++ version_metadata/exp_semantic_rules.py
@@ -17,3 +17,30 @@ def rule_miRNA_smRNA_strategy(attributes):
 		return validation_status
 	except Exception as e:
 		return False
+
+def rule_chip_umi_read_structure(attributes):
+	""" {
+		"applies" : ["chip-seq", "experiment_type"],
+		"description" : "If 'umi_enabled' is 'true', then 'umi_read_structure' must be valid"
+	} """
+	def read_struct_valid(struct):
+		if struct is None: return False
+		if not struct.strip(): return False
+		else:
+			return True
+	if verbose:
+		print('#__rule:', rule_chip_umi_read_structure.__name__,)
+	
+	if not attributes['experiment_type'] in ['ChIP-Seq']:
+		if verbose: print('#__rule:', rule_chip_umi_read_structure.__name__, '__does_not_apply__') 
+		return True
+	else:
+		print('#__rule:', rule_chip_umi_read_structure.__name__) 
+
+	is_umi = attributes.get('umi_enabled')
+	if not is_umi in ['true', 'false']:
+		return False
+	elif is_umi in  ["true"]:
+		return read_struct(attributes.get("umi_read_structure"))
+	else:
+		return True
