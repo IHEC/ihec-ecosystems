ffa9c81f49d8df595397ed018f0951a78efc42b1
zxenia <ksenia1zaytseva@gmail.com>
2020-11-04 18:44:23 -0500

    add caching for EpiRR responses style fixes

--- IHEC_Data_Hub/validateHub.py
+++ IHEC_Data_Hub/validateHub.py
@@ -8,11 +8,12 @@ import re
 import logging
 import urllib.request
 from urllib.error import HTTPError
+import requests
 from ontology_lookup import OntologyLookup
 
 
 def main(argv):
-    """Command line way to valide a data hub"""
+    """ Command line way to valide a data hub. """
 
     FORMAT = '%(message)s'
     logging.basicConfig(format=FORMAT)
@@ -37,7 +38,7 @@ def main(argv):
         elif opt == '--epirr':
             validate_epirr = True
 
-    #JSON to be validated is mandatory, otherwise, display options
+    # JSON to be validated is mandatory, otherwise, display options
     if json_filename == '':
         printHelp()
         exit()
@@ -58,13 +59,13 @@ def main(argv):
 
 
 def jsonschemaErrorReport(jsonObj, schema_version):
-    """ Return error report"""
+    """ Return error report. """
 
     schema_file = os.path.dirname(os.path.realpath(__file__)) + '/../schemas/json/' + schema_version + '/hub.json'
 
     with open(schema_file) as jsonStr:
         json_schema = json.load(jsonStr)
-    v = jsonschema.Draft7Validator(json_schema)
+    v = jsonschema.Draft7Validator(json_schema, format_checker=jsonschema.FormatChecker())
     errors = [e for e in v.iter_errors(jsonObj)]
     logging.getLogger().info('Total errors: {}'.format(len(errors)))
     print("--------------------------------------------------")
@@ -88,26 +89,26 @@ def printHelp():
 
 
 def validateJson(jsonObj, schema_file, validate_epirr, is_loose_validation):
-    """Validate a data hub against the IHEC Data Hub Schema"""
+    """ Validate a data hub against the IHEC Data Hub Schema. """
 
-    #Syntactic validation
+    # Syntactic validation
     logging.getLogger().info('Validating syntax against JSON Schema...')
     with open(schema_file) as jsonStr:
         json_schema = json.load(jsonStr)
     jsonschema.validate(jsonObj, json_schema, format_checker=jsonschema.FormatChecker())
     logging.getLogger().info('Schema validation OK.')
 
-    #Semantic validation
+    # Semantic validation
     logging.getLogger().info('Validating Datasets object...')
     sample_list = jsonObj['samples'].keys()
     validateDatasets(jsonObj['datasets'], sample_list, is_loose_validation)
     logging.getLogger().info('Datasets validation passed.')
 
-    #EpiRR validation
+    # EpiRR validation
     if validate_epirr:
         logging.getLogger().info('Validating metadata against EpiRR records...')
         validateEpirr(jsonObj)
-        logging.getLogger().info('EpiRR validation passed.')
+        logging.getLogger().info('EpiRR validation finished.')
 
     # TF Target validation against HGNC
     datasets = jsonObj.get('datasets')
@@ -125,12 +126,12 @@ def validateJson(jsonObj, schema_file, validate_epirr, is_loose_validation):
 
 
 def validateDatasets(datasets, sample_list, is_loose_validation):
-    """Validate that dataset objects properties are OK."""
+    """ Validate that dataset objects properties are OK. """
 
     for dn in datasets:
         dataset = datasets[dn]
 
-        #Does the sample name exist in the 'samples' dictionary?
+        # Does the sample name exist in the 'samples' dictionary?
         try:
             if isinstance(dataset['sample_id'], list):
                 for sample_id in dataset['sample_id']:
@@ -142,10 +143,9 @@ def validateDatasets(datasets, sample_list, is_loose_validation):
         except Exception as e:
             logging.getLogger().error('Dataset is linked to an unknown sample_id. (sample_id="%s" does not exist in "samples" dictionary).' % (e))
 
-
         for track_type in dataset['browser']:
 
-            #Display warning if tracks are of an unknown type, in strict validation mode.
+            # Display warning if tracks are of an unknown type, in strict validation mode.
             if not is_loose_validation:
                 if track_type not in ["signal_unstranded", "signal_forward", "signal_reverse", "peak_calls", "methylation_profile", "contigs"]:
                     logging.getLogger().error('Track type is not in known types: "%s"' % (track_type))
@@ -163,7 +163,7 @@ def validateDatasets(datasets, sample_list, is_loose_validation):
 
 
 def validateMd5(md5):
-    """Verify length and character content for provided MD5 checksum."""
+    """ Verify length and character content for provided MD5 checksum. """
 
     m = re.search('^[0-9abcdefABCDEF]{32}$', md5)
     try:
@@ -173,29 +173,54 @@ def validateMd5(md5):
 
 
 def validatePrimaryTrack(tracks):
-    """If more than one track is provided for a track type, make sure the primary flag is defined, and set to one and only one track."""
+    """ If more than one track is provided for a track type,
+    make sure the primary flag is defined, and set to one and only one track. """
 
     if len(tracks) > 1:
         primary_met = False
         for track in tracks:
 
-            #"primary" property is mandatory when there's more than one track for this type.
+            # "primary" property is mandatory when there's more than one track for this type.
             if not 'primary' in track:
                 raise Exception('Missing "primary" property.')
 
-            #Only one track should be set as "primary"
+            # Only one track should be set as "primary"
             if track['primary']:
                 if primary_met:
                     raise Exception('Already met a "primary" track for this track type.')
                 primary_met = True
 
-        #One of the track has to be "primary"
+        # One of the track has to be "primary"
         if not primary_met:
             raise Exception('No track defined as "primary: true" for this track type.')
 
 
+def api_call_epirr(epirr_id):
+    """ Call EpiRR api to check if EpiRR record exists. """
+    try:
+        base_url = 'http://www.ebi.ac.uk/vg/epirr/view/'
+        r = requests.get(base_url + epirr_id, headers={'accept': 'application/json'})
+        r.raise_for_status()
+        response = json.loads(r.content.decode('utf-8'))
+        return response
+
+    except requests.exceptions.HTTPError as e:
+        logging.getLogger().warning("The record {} is not found ({})".format(epirr_id, e))
+
+
+# cache EpiRR response to avoid redundant calls
+cache = dict()
+
+
+def api_with_cache(epirr_id):
+    """ Cache EpiRR response in cache dict. """
+    if epirr_id not in cache:
+        cache[epirr_id] = api_call_epirr(epirr_id)
+    return cache[epirr_id]
+
+
 def validateEpirr(jsonObj):
-    """Ensure that IHEC Data Hub metadata matches with EpiRR record."""
+    """ Ensure that IHEC Data Hub metadata matches with EpiRR record. """
 
     print()
 
@@ -216,38 +241,27 @@ def validateEpirr(jsonObj):
         else:
             ds_names = [dataset['sample_id']]
 
-        #If dataset has an EpiRR id, validate that metadata matches
+        # If dataset has an EpiRR id, validate that metadata matches
         if 'reference_registry_id' in exp_attr:
             epirr_id = exp_attr['reference_registry_id'][0]
             logging.getLogger().info('Validating dataset "%s" against EpiRR record "%s"...' % (dataset_name, epirr_id))
 
+            epirr_json = api_with_cache(epirr_id)
+            if epirr_json:
+                epirr_sample_metadata = epirr_json['meta_data']
 
-            try:
-                r = urllib.request.Request('http://www.ebi.ac.uk/vg/epirr/view/' + epirr_id,
-                                           headers={"Accept": "application/json"})
-                response = urllib.request.urlopen(r).read()
-            except HTTPError as e:
-                if e.code == 404:
-                    logging.getLogger().warning("The record {} is not found ({})".format(epirr_id, e))
-                else:
-                    logging.getLogger().warning("Unexpected error {}".format(e))
-                continue
+                # Validate that each sample that this dataset refers to holds the correct metadata
+                for ds_name in ds_names:
+                    hub_sample_metadata = samples[ds_name]
+                    logging.getLogger().debug('Validating sample "%s" properties...' % (ds_name))
+                    validateSample(epirr_sample_metadata, hub_sample_metadata, dataset_name)
 
-            epirr_json = json.loads(response.decode('utf-8'))
-            epirr_sample_metadata = epirr_json['meta_data']
+                # Case-insensitive check that each experiment that has an EpiRR id is actually registered at EpiRR.
+                raw_data_per_exp = {rd['experiment_type'].lower(): rd for rd in epirr_json['raw_data']}
+                if exp_name.lower() not in raw_data_per_exp:
+                    logging.getLogger().error('-Experiment "%s" could not be found in EpiRR record %s.' % (exp_name, epirr_id))
 
-            #Validate that each sample that this dataset refers to holds the correct metadata
-            for ds_name in ds_names:
-                hub_sample_metadata = samples[ds_name]
-                logging.getLogger().debug('Validating sample "%s" properties...' % (ds_name))
-                validateSample(epirr_sample_metadata, hub_sample_metadata, dataset_name)
-
-            #Case-insensitive check that each experiment that has an EpiRR id is actually registered at EpiRR.
-            raw_data_per_exp = {rd['experiment_type'].lower(): rd for rd in epirr_json['raw_data']}
-            if exp_name.lower() not in raw_data_per_exp:
-                logging.getLogger().error('-Experiment "%s" could not be found in EpiRR record %s.' % (exp_name, epirr_id))
-
-            print()
+                print()
 
 
 def validateSample(epirr_sample_metadata, hub_sample_metadata, dataset_name):
