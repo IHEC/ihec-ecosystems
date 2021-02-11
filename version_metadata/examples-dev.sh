5a7bec7bda1c2f40d360a89156bc771bf0c57e66
sitag <sgakkhar@bcgsc.ca>
2021-02-10 21:12:32 -0800

    fixing for merge

---
+++ version_metadata/examples-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+
+
+
+examples() {
+	cd ..
+	m="version_metadata"
+    python -m $m -sample -dev -out:$m/examples/samples.versioned.xml $m/examples/sample_1.0_1.1.xml -overwrite-outfile;
+	python -m $m -sample -dev -out:$m/examples/samples.versioned.xml $m/examples/samples.xml -overwrite-outfile; 
+	python -m $m -sample -dev -out:$m/examples/samples.with_one_invalid.versioned.xml $m/examples/samples.with_one_invalid.xml -overwrite-outfile ;
+	python -m $m -sample -dev -out:$m/examples/samples.with_multiple_valid_invalid.versioned.xml $m/examples/samples.with_multiple_valid_invalid.xml -overwrite-outfile ;
+	python -m $m -experiment -dev $m/examples/experiment.xml -out:$m/examples/experiment.validated.xml -overwrite-outfile ;
+	python -m $m  -experiment -dev $m/examples/experiment.some_invalid.xml -out:$m/examples/experiment.some_invalid.validated.xml -overwrite-outfile ; 
+	cd -
+}
+
+examples 2>&1 
