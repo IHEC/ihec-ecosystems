1e6d027a99ae3416d99328de31c97f00159a035f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-21 11:25:11 +0800

    Delete IHEC_Data_Hub directory

--- IHEC_Data_Hub/__init__.py
+++
@@ -1,2 +0,0 @@
-from .validateHub import jsonschemaErrorReport, validateHgncSymbol, symbolStatus
-from .ontology_lookup import *
--- IHEC_Data_Hub/converter_to_UCSC/Templater/__init__.py
+++
@@ -1,7 +0,0 @@
-from MinimalPyUtils import *
-from Config import Config, Parser
-from Hub import Hub
-from DataHub import DataHub
-
-if __name__ == '__main__':
-	pass 
